4a2860a432efef47eb3b899cae6fcf6f8248411d
Matej Sprogar <46596387+matejsprogar@users.noreply.github.com>
2025-02-11 12:44:29 +0100

    rf

--- helpers.h
+++ helpers.h
@@ -1,20 +1,20 @@
 /*
  * Copyright 2024 Matej Sprogar <matej.sprogar@gmail.com>
+ * 
+ * This file is part of AITB - Artificial Intelligence Testbed.
  *
- * This file is part of HLITB - Human Like Intelligence Testbed.
- *
- * HLITB is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR signal PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * */
 #pragma once
 
@@ -54,10 +54,7 @@ namespace sprogar {
             template <BitProvider T>
             size_t count_matching_bits(const T& a, const T& b)
             {
-                size_t matching_bits = 0;
-                for (size_t i = 0; i < T::size(); ++i)
-                    matching_bits += a[i] == b[i];
-                return matching_bits;
+                return std::ranges::count_if(std::views::iota(0ull, T::size()), [&](size_t i) { return a[i] == b[i]; });
             }
         }
     }
